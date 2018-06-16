8f02e87ceb0991a86d4b065ba70327bf14420f38
manozoys <mano.kumar@vvdntech.in>
2018-06-16 16:58:23 +0530

    Ex-1

---
+++ datatypes.py
@@ -0,0 +1,8 @@
+#!/usr/bin/python
+Datatype = raw_input("Enter the input:")
+print "Integer", int(Datatype)
+print "Float", float(Datatype)
+print "String", str(Datatype)
+print "complex", complex(Datatype)
+print "List", list(Datatype)
+print "Tuple", tuple(Datatype)
