703c9ed32c0a61283c8886a57cb55323b5c058c6
manozoys <mano.kumar@vvdntech.in>
2018-06-16 17:46:34 +0530

    Removed unwanted files

--- arithmetic operations.py
+++
@@ -1,7 +0,0 @@
-#!/usr/bin/python
-number1 = float(raw_input("Enter the Number1:"))
-number2 = float(raw_input("Enter the Number2:"))
-print "\nAddition:", number1 + number2
-print "Subtraction:", number1 - number2
-print "Multiplication:", number1 * number2
-print "Division:", number1 / number2
--- datatypes.py
+++
@@ -1,8 +0,0 @@
-#!/usr/bin/python
-Datatype = raw_input("Enter the input:")
-print "Integer", int(Datatype)
-print "Float", float(Datatype)
-print "String", str(Datatype)
-print "complex", complex(Datatype)
-print "List", list(Datatype)
-print "Tuple", tuple(Datatype)
